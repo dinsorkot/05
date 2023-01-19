bb621fd80956124c7e27edd9b26dda18ebb4e2f5
dinsorkot <mju372ohm@gmail.com>
2023-01-19 18:24:39 +0700

    เอาใหม่

--- src/components/card.vue
+++ src/components/card.vue
@@ -6,7 +6,7 @@ import G_like from './G_like.vue';
 <template>
     <div class="card col-3 m-3 p-2 pic" style="max-height: 480px; overflow:scroll;" v-for="n in 6" :key="n">
         {{ n }}
-        <img :src="`./assets/img/pic${n}.jpg`" :alt="`pic${n}`" >
+        <img :src="`../src/assets/img/pic${n}.jpg`" :alt="`pic${n}`" >
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
